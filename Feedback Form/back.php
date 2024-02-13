691e69b914d5348a38cdfa4821a00c719557e152
Bakhadyr04 <bahadyr2004@mail.ru>
2024-02-13 13:26:19 +0300

    Hello, world!

---
+++ Feedback Form/back.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <img src="./img/logo.png" alt="Mospolytech">
+    </header>
+    <textarea name="" id="" cols="30" rows="10">
+        <?php
+            print_r(get_headers('https://getbootstrap.com/docs/5.3/forms/overview/#overview'));
+        ?>
+    </textarea>
+    <a href="index.html">Back</a>
+</body>
+</html
